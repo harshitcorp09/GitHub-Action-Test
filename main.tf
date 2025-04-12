f6f40e2cccec61efd74e434b5676282cd8c01c41
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2025-04-12 12:11:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform { 
+  cloud { 
+    hostname = "tfe-docker-fdo2.harshit-dixit.sbx.hashidemos.io" 
+    organization = "Harshit-Org" 
+
+    workspaces { 
+      name = "Github-Action-Test" 
+    } 
+  } 
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "tfe-gha-demo-bucket-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
